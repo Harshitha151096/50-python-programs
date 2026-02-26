2aeb24e4963414c32a7fb0625935440d5697e2fc
Arjun <harshithaprakash15@gmail.com>
2026-02-26 17:14:14 +0530

    add python files

---
+++ Pg4.py
@@ -0,0 +1,7 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+
+average =(int(num1) + int(num2))
+
+print('average:{0} '.format(average))
+# print(f'average:{average}')
---
+++ Pg6.py
@@ -0,0 +1,5 @@
+firstexam = input("our first exam : ")
+secondexam = input('your second exam : ')
+thirdexam = input('your third exam : ')
+average =(float(firstexam)+float(secondexam)+float(thirdexam))/3
+print(f"average :{average}")
